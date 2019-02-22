157a77c20e9de43164b1d61d22ae2684bafa823d
Richie <Richie_t98@Hotmail.com>
2019-02-21 23:44:22 -0500

    Card class

---
+++ src/githubtesting/Card.java
@@ -0,0 +1,37 @@
+package githubtesting;
+
+public class Card {
+
+	private int number;
+	private String type;
+
+	public Int getNumber() {
+		// TODO - implement Card.getNumber
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param number
+	 */
+	public void setNumber(Int number) {
+		// TODO - implement Card.setNumber
+		throw new UnsupportedOperationException();
+	}
+        public void addCards(Card cardOne, Card cardTwo){
+        
+        }
+
+	public String getType() {
+		return this.type;
+	}
+
+	/**
+	 * 
+	 * @param type
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+
+}
